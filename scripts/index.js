d6c55a7dd3f709f23892fb51dea9025e72989c05
Matthew Rawlings <matthew.rawlings6@gmail.com>
2021-07-18 11:16:49 +0200

    shortened fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,5 @@
-const fetchProducts = () => {
-  fetch('https://fedsa-project-1.herokuapp.com/project-1/products')
-    .then((response) => response.json())
-    .then((data) => data);
-};
-
-const products = fetchProducts();
+const products = fetch(
+  'https://fedsa-project-1.herokuapp.com/project-1/products',
+)
+  .then((response) => response.json())
+  .then((data) => data);
